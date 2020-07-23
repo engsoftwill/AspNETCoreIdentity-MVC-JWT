28fb982c3a095c34b1aadb40848c55972d6d72d7
engsoftwill <engeletwill@gmail.com>
2020-07-23 16:34:13 -0300

    Final da Seção 2

---
+++ WebApp.Identity/Controllers/HomeController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApp.Identity.Models;
+
+namespace WebApp.Identity.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly UserManager<MyUser> _userManager;
+        public HomeController(UserManager<MyUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        
+
+   
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid) // se password e confirme password for validado
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+
+                if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+                {
+                    var identity = new ClaimsIdentity("cookies");
+                    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id));
+                    identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName));
+
+                    await HttpContext.SignInAsync("cookies", new ClaimsPrincipal(identity));
+
+                    return RedirectToAction("About");
+                }
+                
+                ModelState.AddModelError("", "Usuario ou senha invalidos");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid) // se password e confirme password for validado
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+                if (user == null)
+                {
+                    user = new MyUser
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        UserName = model.UserName
+                    };
+
+                    var result = await _userManager.CreateAsync(
+                        user, model.Password);
+                }
+                    return View("Success");
+            }
+            return View();
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Success()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApp.Identity/MyUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Identity
+{
+    public class MyUser
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string NormalizedUserName { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
+
+
+/*
+ * CREATE TABLE [dbo].[Users](
+	[Id] [nvarchar] (450) NOT NULL,
+	[UserName] [nvarchar] (256) NULL,
+	[NormalizedUserName] [nvarchar] (256) NULL,
+	[PasswordHash] [nvarchar] (max) NULL,
+CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED(
+	[Id]  ASC
+
+))
+*/
